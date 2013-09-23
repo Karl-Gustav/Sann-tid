6e0143ecbabb23fe542d8e650d543d56ac89ef6e
Karl Gustav Røksund <kkgg84+git@gmail.com>
2013-09-23 21:38:35 +0200

    Ugly post fuction

--- python/QuestionHandler.py
+++ python/QuestionHandler.py
@@ -1,26 +1,38 @@
 from google.appengine.ext import webapp
 from google.appengine.ext import db
+import logging
 from lib.models import QuestionModel
-from lib.decorators import httpCode_loginRequired
+import json
+from lib.decorators import httpCode
 from lib.HTTPExceptions import raise400, HTTP409
 
 class QuestionHandler(webapp.RequestHandler):
 	def get(self, placeholder): #get
 		self.response.out.write( "[%s]" % ",".join(x.toJSON() for x in QuestionModel.all() ) )
 
-	def post(self, questionId): #update
-		questionModel = QuestionModel.get(questionId)
-		questionModel.question = self.request.get('question') or raise400('Question can\'t be empty!')
-		questionModel.answer = self.request.get('answer') or raise400('Question can\'t be empty!')
+	@httpCode
+	def put(self, placeholder): #create
+		questionModel = QuestionModel()
+		body = json.loads(self.request.body)
+		logging.info("test"+ str(self.request))
+		questionModel.question = body['question'] or raise400('Question can\'t be empty!')
+		questionModel.answer = body['answer'] or raise400('Question can\'t be empty!')
 		questionModel.put()
+
 		self.response.out.write(questionModel.toJSON())
 
-	def post(self): #create
-		questionModel = QuestionModel()
-		questionModel.question = self.request.get('question') or raise400('Question can\'t be empty!')
-		questionModel.answer = self.request.get('answer') or raise400('Question can\'t be empty!')
+	@httpCode
+	def post(self, questionId): #update
+		logging.info("test2"+str(self.request))
+		questionModel = QuestionModel.get(questionId.strip())
+		if self.request.headers['Content-Type'] == 'application/json':
+			body = json.loads(self.request.body)
+			questionModel.question = body['question'] or raise400('Question can\'t be empty!')
+			questionModel.answer = body['answer'] or raise400('Question can\'t be empty!')
+		elif self.request.headers['Content-Type'] == 'application/x-www-form-urlencoded':
+			questionModel.question = self.request.get('question') or raise400('Question can\'t be empty!')
+			questionModel.answer = self.request.get('answer') or raise400('Question can\'t be empty!')
 		questionModel.put()
-
 		self.response.out.write(questionModel.toJSON())
 
 	def delete(self, questionId):
