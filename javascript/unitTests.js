62c362c1ffab459bf98e0bffff1274c03790b47a
Karl Gustav Roksund <karl.gustav.roksund@accenture.com>
2013-11-25 16:23:55 +0100

    Simplefied the project by removing unnecessary suff

--- javascript/unitTests.js
+++
@@ -1,6 +0,0 @@
-describe('dateService momentToMinutesAndSeconds', function(){
-	beforeEach(module('testApp'));
-	it('should return 60 min when given 3600 sec', inject(function(dateService){
-		expect(dateService.momentToMinutesAndSeconds(3600).toBe([60,0]))
-	}))
-})
--- main.py
+++
@@ -1,7 +0,0 @@
-import webapp2
-from python.QuestionHandler import QuestionHandler
-
-
-app = webapp2.WSGIApplication([
-		(r'/rest/questions/?([\w\-]+)?', QuestionHandler)
-	], debug=True)
