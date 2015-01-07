bb89d9f1ed1181fed485fd59a78ad3d1b9edb355
Karl Gustav Røksund <kkgg84+git@gmail.com>
2015-01-07 20:59:07 +0100

    Changed the findStopController to auto search on type
    This is so that it's quicker for the user to find the stop he's
    looking for.

--- html/app/controllers/findStopController.js
+++ html/app/controllers/findStopController.js
@@ -1,14 +1,27 @@
 'use strict';
 
-angular.module('app').controller("findStopController", function($scope, findPlaceResource, $location){
-    $scope.name = $location.search().query
-    $scope.search = function(){
-        $location.search('query', $scope.name)
-        findPlaceResource.query({placeName: $location.search().query}, function(data){
-            $scope.data = data
-        }, function(error){
-            $scope.error = error
-            $log.error("Find stop query failed:", error)
-        })
+angular.module('app').controller("findStopController", function($scope, findPlaceResource, $location, $log){
+    $scope.searchString = $location.search().query;
+    $scope.search = search;
+
+    if ($location.search().query) {
+        search();
+    }
+
+    $scope.$watch('searchString', function (newVal, oldVal) {
+        $location.search('query', newVal);
+    })
+
+    $scope.$on('$routeUpdate', search);
+
+    function search () {
+        if ($location.search().query && $location.search().query.length > 0) {
+            findPlaceResource.query({placeName: $location.search().query}, function(data){
+                $scope.data = data
+            }, function(error){
+                $scope.error = error
+                $log.error("Find stop query failed:", error)
+            })
+        }
     }
 })
--- html/app/routes.js
+++ html/app/routes.js
@@ -1,13 +1,13 @@
 angular.module('app').config(function($routeProvider){
-            $routeProvider
-                    .when('/',{
-                                                controller: "findStopController",
-                                            templateUrl: "partials/chooseStop.html",
-                                            reloadOnSearch: false
-                                        })
-                .when('/showRealtime/:stopId',{
-                                            controller: "realtimeController",
-                                        templateUrl: "partials/showRealtime.html"
-                                    })
-                                .otherwise({redirectTo: "/"})
-            })
+    $routeProvider
+        .when('/',{
+            controller: "findStopController",
+            templateUrl: "partials/chooseStop.html",
+            reloadOnSearch: false
+        })
+        .when('/showRealtime/:stopId',{
+            controller: "realtimeController",
+            templateUrl: "partials/showRealtime.html"
+        })
+        .otherwise({redirectTo: "/"})
+})
